9ae1b0ee39aafce43833384e2f36969a56693719
chrisbgn <christianbogarin@gmail.com>
2022-02-07 23:34:12 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 console.log('leyendo js');
-function destinoCambio(event) {
+function destinoCambio(value) {
     console.log('dentro de la funcion');
-    var destino = event.target.value;
-    console.log('destino >>> ', destino);
-    if(destino == 'CABA'){
+    console.log('destino >>> ', value);
+    if(value == 'CABA'){
         console.log('adentro del if el peluca sapbeeee');
         alert('No necesitas presentar el pase sapbeeeeee');
     }
