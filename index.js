1c0bbf0832b9501c6bb01f11676f4e8e98268ce6
chrisbgn <christianbogarin@gmail.com>
2022-02-08 01:08:39 -0300

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function destinoCambio(value) {
     var origen = document.querySelector("#origenId");
     var destino = document.querySelector("#destinoId");
     console.log('origen value => ', origen.value);
-    if(origen.value == 'CABA' || destino.value == 'CABA'){
+    if((origen.value == 'CABA' && destino.value != '') || (destino.value == 'CABA' && origen.value != '')){
         console.log('adentro del if el peluca sapbeeee');
         alert('No necesitas presentar el pase sapbeeeeee');
         
