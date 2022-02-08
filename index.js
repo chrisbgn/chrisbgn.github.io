c38cf1ebce0f7f64fe9f1e650998b4036374b428
chrisbgn <christianbogarin@gmail.com>
2022-02-08 00:44:41 -0300

    Update index.js

--- index.js
+++ index.js
@@ -2,10 +2,15 @@ console.log('leyendo js');
 function destinoCambio(value) {
     console.log('dentro de la funcion');
     console.log('destino >>> ', value);
-    if(value == 'CABA'){
+    var origen = document.querySelector("#origenId");
+    var destino = document.querySelector("#destinoId"); 
+    if(origen == 'CABA' || destino == 'CABA'){
         console.log('adentro del if el peluca sapbeeee');
         alert('No necesitas presentar el pase sapbeeeeee');
-        var origen = document.querySelector("#origenId");
         console.log('origen value => ', origen.value);
+    } else if(origen == undefined || origen == '' || destino == undefined || destino == '') {
+    } else {
+        alert('Fuiste causa, entregando el paseeee');
     }
+        
 }
