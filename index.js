f62a596ec185f3fb2eadf19acb802eb9f3be56a9
chrisbgn <christianbogarin@gmail.com>
2022-02-07 23:08:03 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
+console.log('leyendo js');
 function destinoCambio(event) {
+    console.log('dentro de la funcion');
     var destino = event.target.value;
     console.log('destino >>> ', destino);
     if(destino == 'CABA'){
