76e324bae5fa25e3cf340b26020e3bfcc1f5d6b2
chrisbgn <christianbogarin@gmail.com>
2022-02-07 23:03:03 -0300

    Update index.js

--- index.js
+++ index.js
@@ -4,5 +4,6 @@ function destinoCambio(event) {
     var destino = event.target.value;
     if(destino == 'CABA'){
         noNecesita = true;
+        alert('No necesitas presentar el pase sapbeeeeee');
     }
 }
